18bedaeb94df20f10df5330a5a2df91d4acfc1f0
Sean Khanna <sean6beechdrive@hotmail.co.uk>
2018-11-21 19:17:06 +0000

    T8E1&2&3&4

---
+++ T08/T08E02/shaders.hlsl
@@ -0,0 +1,32 @@
+Texture2D texture0;
+SamplerState sampler0;
+
+cbuffer CBuffer0
+{
+	matrix WVPMatrix;	// 64 bytes
+	float red_fraction; // 4 bytes
+	float scale;		// 4 bytes
+	float2 packing;		// 8 bytes
+}//total 80 bytes
+
+struct VOut
+{
+	float4 position : SV_POSITION;
+	float4 color	: COLOR;//Note the spelling of this and all instance below
+	float2 texcoord : TEXCOORD;
+};
+
+VOut VShader(float4 position : POSITION, float4 color : COLOR, float2 texcoord : TEXCOORD)
+{
+	VOut output;
+	output.position = mul(WVPMatrix, position);
+	output.color = color;
+	output.texcoord = texcoord;
+
+	return output;
+}
+
+float4 PShader(float4 position : SV_POSITION, float4 color : COLOR, float2 texcoord : TEXCOORD) : SV_TARGET
+{
+	return color * texture0.Sample(sampler0,texcoord);
+}
