d3c6332dbf8636ad2d1979bc3b209e11935f8d76
Sean Khanna <sean6beechdrive@hotmail.co.uk>
2018-12-13 13:28:25 +0000

    T17 started

---
+++ T17/T17E01/SceneNode.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <d3d11.h>
+#include <dxgi.h>
+#include <d3dx11.h>
+#include <Windows.h>
+#include <dxerr.h>
+#define _XM_NO_INTRINSICS_
+#define _XM_NO_ALIGNMENT
+#include <xnamath.h>
+#include<Xinput.h>
+#include<WinUser.h>
+#include<windowsx.h>
+#include "Model.h"
+#include"camera.h"
+
+class SceneNode
+{
+public:
+	SceneNode();
+	~SceneNode();
+	void SetModel(Model*model);
+	void SetCamera(camera*Camera);
+	void SetPos(float xpos, float ypos, float zpos);
+	XMVECTOR GetPos();
+	void SetRotation(float xAngle, float yAngle, float zAngle);
+	XMVECTOR GetRotation();
+	void SetScale(float scale);
+	float GetScale();
+	void addChildNode(SceneNode* n);
+	bool detachNode(SceneNode* n);
+	void execute(XMMATRIX *world, XMMATRIX* view, XMMATRIX* projection);
+	void IncPos(float xAmount, float yAmount, float zAmount);
+	void IncRotation(float xAmount, float yAmount, float zAmount);
+	void IncScale(float scaleAmount);
+	void LookAt_XZ(float xWorld, float zWorld);
+	void LookAt_XYZ(float xWorld, float yWorld, float zWorld);
+	void MoveForward(float distance);
+	void MoveForwardIncY(float distance);
+	vector<SceneNode*> GetChildren();
+private:
+	Model* m_p_model;
+	camera* m_p_camera;
+	vector<SceneNode*> m_children;
+	float m_x, m_y, m_z;
+	float m_xAngle, m_yAngle, m_zAngle;
+	float m_scale;
+
+};
+
---
+++ T17/T17E01/camera.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<d3d11.h>
+#define _XM_NO_INTRINSICS_
+#define _XM_NO_ALIGNMENT
+#include<xnamath.h>
+#include<math.h>
+class camera
+{
+public:
+	camera(float x, float y, float z, float camera_rotation_pitch, float camera_rotation_yaw);
+	~camera();
+	void RotateAroundX(float rotation_amount);
+	void RotateAroundY(float rotation_amount);
+	void Forward(float distance_amount);
+	void Strafe(float distance_amount);
+	void Up(float distance_amount);
+	XMMATRIX GetViewMatrix();
+	XMVECTOR GetPos();
+	XMVECTOR GetRot();
+private:
+	float m_x, m_y, m_z, m_dx, m_dy, m_dz, m_camera_rotation_yaw, m_camera_rotation_pitch;
+	XMVECTOR m_position, m_lookat, m_up;
+};
+
